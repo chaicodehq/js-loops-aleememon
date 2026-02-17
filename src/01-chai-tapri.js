1f80850fc8c0c1c7674ab4b9310549fcf10a59c5
aleememon <am7359681@gmail.com>
2026-02-18 03:18:52 +0500

    solve problem no 01-chai-tapri.js

--- src/01-chai-tapri.js
+++ src/01-chai-tapri.js
@@ -28,4 +28,17 @@
  */
 export function chaiTapriRevenue(customers) {
   // Your code here
+  if (!Number.isInteger(customers) || customers <= 0)
+    return { totalChai: 0, totalRevenue: 0 };
+  // let totalChai;
+  let adarkChai = 0;
+  let normalChai = 0;
+  for (let i = 1; i <= customers; i++) {
+    if (i % 3 === 0) {
+      adarkChai = adarkChai + 15;
+    } else {
+      normalChai = normalChai + 10;
+    }
+  }
+  return { totalChai: customers, totalRevenue: adarkChai + normalChai };
 }
